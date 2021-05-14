f2355fbc512407dfbbadcf1d0a49fc9a4daaf9b2
Warachia <fsfsfs45@gmail.com>
2021-05-14 21:30:26 +0900

    fix

--- codes/src/necrorankings/HtmlGenerator.java
+++ codes/src/necrorankings/HtmlGenerator.java
@@ -1730,10 +1730,10 @@ public class HtmlGenerator extends DefaultHandler{
                 	p.println(ranktag(player.phasingspeed[i], 3, i, "speed", Player.csecToString(player.extratime[3][i])));
                 	p.println(ranktag(player.mysteryspeed[i], 4, i, "speed", Player.csecToString(player.extratime[4][i])));
                 	p.println("<td>" + "<img src=\"icons/" + i + ".jpg\">" + "</td>");
-                	p.println(exscoreranktag(player, 4, i, scorethreshold(i)));
-                	p.println(exscoreranktag(player, 4, i, scorethreshold(i)));
-                	p.println(exscoreranktag(player, 4, i, scorethreshold(i)));
-                	p.println(exscoreranktag(player, 4, i, (int) (scorethreshold(i)*0.8)));
+                	p.println(exscoreranktag(player, 0, i, scorethreshold(i)));
+                	p.println(exscoreranktag(player, 1, i, scorethreshold(i)));
+                	p.println(exscoreranktag(player, 2, i, scorethreshold(i)));
+                	p.println(exscoreranktag(player, 3, i, (int) (scorethreshold(i)*0.8)));
                 	p.println(exscoreranktag(player, 4, i, (int) (scorethreshold(i)*0.8)));
                 	p.println(ranktag(player.deathless[i], -1, i, "deathless", player.clearcount(i)));
                     p.println("</tr>");
